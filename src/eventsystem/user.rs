671d8fbb9ca5dfb4b0ed8201d0ff495da5f15aa5
found-cake <me@foundcake.kr>
2026-01-24 18:41:06 +0900

    feat: add Defer verdict in UserVerdict enum

--- src/eventsystem/user.rs
+++ src/eventsystem/user.rs
@@ -3,12 +3,12 @@ pub struct UserVerdict {
     pub op_id: u64,
     pub verdict: Verdict,
     pub reason_code: u32,
+    pub ttl_ms: Option<u32>
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Verdict {
     Allow = 0,
     Deny = 1,
-    // 좀더 고민중...
-    //Defer = 2,
+    Defer = 2,
 }
